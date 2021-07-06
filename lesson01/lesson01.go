a74bef44ec6f487a37ef841d0502af6564808ff3
lunarnuts <n.kuandyk1995@gmail.com>
2021-07-06 16:53:03 +0600

    update :cool:

--- lesson01/lesson01.go
+++ lesson01/lesson01.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji"
 )
 
 func main() {
-	fmt.Print("Hello, world ")
-	emoji.Println(":relaxed:")
+	message := emoji.Sprint("Hello, world :relaxed:")
+	emoji.Println(message)
 }
