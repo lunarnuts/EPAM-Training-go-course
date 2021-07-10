efebcc5d9a1a2d5798d6ea111171320f027abc1e
Nursultan <lunarnuts@Nursultan-FX505.localdomain>
2021-07-10 16:17:18 +0600

    initial

---
+++ lesson02/fibbonacci/go.mod
@@ -0,0 +1,3 @@
+module github.com/lunarnuts/go-course/tree/lesson02/fibbonacci
+
+go 1.16
---
+++ lesson02/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/lunarnuts/go-course/tree/lesson02"
+)
+
+func main() {
+	Printer(40)
+}
