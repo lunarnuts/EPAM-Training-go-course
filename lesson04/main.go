8473418baa9c773fea3002d3d838173965b037bc
Nursultan <lunarnuts@Nursultan-FX505.localdomain>
2021-07-16 00:06:49 +0600

    refactored into shapes pkg

--- lesson04/main.go
+++ lesson04/main.go
@@ -2,60 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"github.com/lunarnuts/go-course/tree/lesson04/shapes"
 )
 
-type Shape interface {
-	Area() float64
-	Perimeter() float64
-}
-
-type Circle struct {
-	radius float64
-}
-
-type Rectangle struct {
-	height float64
-	width  float64
-}
-
-func DescribeShape(s Shape) {
-	fmt.Println(s)
-	fmt.Printf("Area: %.2f\n", s.Area())
-	fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
-}
-
-func (r Rectangle) Area() float64 {
-	return r.height * r.width
-}
-
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.height + r.width)
-}
-
-func (r Rectangle) String() string {
-	return fmt.Sprintf("\nRectangle with height %.2f and width %.2f", r.height, r.width)
-}
-
-func (c Circle) Area() float64 {
-	return math.Pi * math.Pow(c.radius, 2)
-}
-
-func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.radius
-}
-
-func (c Circle) String() string {
-	return fmt.Sprintf("\nCircle: radius %.2f", c.radius)
+func DescribeShape(s shapes.Shape) {
+   fmt.Println(s)
+   fmt.Printf("Area: %.2f\n", s.Area())
+   fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
 }
 
 func main() {
-	// choose your own dimensions
-	c := Circle{radius: 8}
-	r := Rectangle{
-		height: 9,
-		width:  3,
-	}
-	DescribeShape(c)
-	DescribeShape(r)
+
+
+   var c shapes.Circle
+   c.SetRadius(8)
+   var r shapes.Rectangle
+   r.SetHeight(9)
+   r.SetWidth(3)
+   DescribeShape(c)
+   DescribeShape(r)
 }
