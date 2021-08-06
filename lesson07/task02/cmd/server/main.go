f58e6ae82d2ee5a87c6acb4d37bb517f35e4e663
lunarnuts <n.kuandyk1995@gmail.com>
2021-08-06 17:20:24 +0600

    fix

---
+++ lesson07/task02/cmd/server/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/lunarnuts/go-course/tree/lesson07/task02/cmd/server/db"
+	"github.com/lunarnuts/go-course/tree/lesson07/task02/common/transport"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement transport.UserSearcher.
+type server struct {
+	transport.UnimplementedUserRegisterServer
+}
+
+// Search implements transport.UserSearcher
+func (s *server) UserRegister(ctx context.Context, in *transport.UserRegisterRequest) (*transport.UserRegisterResponse, error) {
+	log.Printf("Received: %v", in)
+	if db.CheckIfExists(in) {
+		log.Printf("User already in database")
+		return nil, fmt.Errorf("User already exist ")
+	}
+	db.AddToDB(in)
+	response := &transport.UserRegisterResponse{
+		Result: db.Data.Map[in.GetName()].GetId(),
+	}
+	return response, nil
+}
+
+// List implements transport.List
+func (s *server) UsersList(ctx context.Context, in *transport.EmptyRequest) (*transport.UsersListResponse, error) {
+	p := db.LoadPersistent()
+	result := &transport.UsersListResponse{
+		Result: p,
+	}
+	return result, nil
+}
+
+func main() {
+	db.Populate()
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	transport.RegisterUserRegisterServer(s, &server{})
+	log.Printf("server listening at %v", listener.Addr())
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
