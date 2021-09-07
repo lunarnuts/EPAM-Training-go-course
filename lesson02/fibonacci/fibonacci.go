6296e8eebd74ae969f42e1186181381d9ee5bf44
lunarnuts <n.kuandyk1995@gmail.com>
2021-09-07 10:18:49 +0600

    fix

--- lesson02/fibonacci/fibonacci.go
+++ lesson02/fibonacci/fibonacci.go
@@ -1,34 +1,26 @@
 package fibonacci
+
 import (
 	"fmt"
-	"strings"
 )
-func Printer(n int) int {
-	one := 0
-	two := 1
-	fmt.Print(one, " ", two, " ")
-	for n > 1 {
-		fib := one + two
-		one, two = two, fib
-		n--
-		fmt.Print(fib, " ")
+
+func Printer(n int) {
+	mp := recursive(n)
+	for _, val := range mp {
+		fmt.Printf("%d ", val)
 	}
-	fmt.Println()
-	return 1
 }
-func Recursive(n int) {
+
+func recursive(n int) []int {
 	mp := make([]int, n+1)
 	for j := range mp {
 		mp[j] = -1
 	}
 	mp[0], mp[1] = 0, 1
 	recurUtil(n, mp)
-	var b strings.Builder
-	for _, i := range mp {
-		fmt.Fprintf(&b, "%d ", i)
-	}
-	fmt.Println(b.String())
+	return mp
 }
+
 func recurUtil(n int, mp []int) int {
 	if mp[n] != -1 {
 		return mp[n]
--- lesson02/main.go
+++ lesson02/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	. "github.com/lunarnuts/go-course/tree/lesson02/fibonacci"
 	"fmt"
+
+	"github.com/lunarnuts/go-course/tree/lesson02/fibonacci"
 )
 
 func main() {
-	defer Recursive(20)
 	defer fmt.Println()
-	defer Printer(20)
+	defer fibonacci.Printer(20)
 	fmt.Println("Fibonacci numbers is a form of sequence, such that")
-	fmt.Println("each number is the sum of two preceeding ones.\n")
+	fmt.Println("each number is the sum of two preceeding ones.")
 }
-
